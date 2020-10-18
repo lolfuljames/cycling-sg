2149ceec14984e6e971b0f537928be1267b77313
James Tan <jamestancheemin98@gmail.com>
2020-10-18 23:59:18 +0800

    Migrate Search from SidePanel to CustomRouteMaker

--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -2,35 +2,9 @@ import React, { useEffect, useState } from 'react'
 import './SidePanel.css';
 import PresetList from './PresetList'
 import CustomRouteMaker from './CustomRouteMaker'
-import { Add,Search,ArrowBack }  from '@material-ui/icons';
+import { Add,ArrowBack }  from '@material-ui/icons';
 import { Button } from '@material-ui/core';
 import axios from 'axios';
-
-/**
- * Get route in GeoJSON format.
- * Returns an axios post request promise.
- * Post request response will be the route (located in data field) in GeoJSON format.
- *  
- * @param {number[][]} coordinates - Array of coordinates including startLocation, endLocation and all POIs (in the format of [lng, lat])
- */
-const getRouteGeoJSON = coordinates => {
-    let url = 'https://api.openrouteservice.org/v2/directions/cycling-regular/geojson';
-    let headers = { Authorization: '5b3ce3597851110001cf62484de2cf33999b4e06a16c3242e98d4443' };
-    return axios.post(url, { coordinates: coordinates }, { headers: headers });
-}
-
-/**
- * Call backend for POIs recommendation.
- * Returns an axios post request promise.
- * Post request response will be an array of POIs object (located in data field) if successful.
- * 
- * -------------------------WARNING--------------------------
- * | This will be billed every time the function is called. |
- * -------------------------WARNING--------------------------
- * 
- * @param {number[][]} coordinates - Array of coordinates which will be used to search for POIs (in the format of [lat, lng])
- */
-const getPOIs = (coordinates, POIType) => axios.post('/poi/poiRec', { coordinates: coordinates, radius: 1500, type: POIType })
 
 export default function SidePanel(props) {
     const [chosenRoute, setChosenRoute] = props.chosenRoute
@@ -62,48 +36,6 @@ export default function SidePanel(props) {
         setPOIType(null);
     }
 
-    function handleSearchCustomRoute() {
-        if (!startLocation) {
-            window.alert("Please enter your start point before proceeding.")
-            return
-        }
-        if (!endLocation) {
-            window.alert("Please enter your end point before proceeding.")
-            return
-        }
-        console.log('Generating route.....')
-
-        let coordinates = chosenPOI.map(place => {
-            return [place.geometry.location.lng, place.geometry.location.lat];
-        });
-        coordinates = [[startLocation.lng, startLocation.lat], ...coordinates ,[endLocation.lng, endLocation.lat]];
-        getRouteGeoJSON(coordinates).then(res => {
-            setChosenRoute(res.data);
-            console.log('Done generating route.');
-            // Add coordinates which will be used to perform nearby search into searchCoordinates. 
-            // --------------------------------------------------WARNING--------------------------------------------------
-            // | Only uncomment the code below when you need it for development as each call will be billed accordingly. |
-            // --------------------------------------------------WARNING--------------------------------------------------
-            if (POIType) {
-                let searchCoordinates = res.data.features[0].geometry.coordinates.map(coordinate => {
-                    return {lat: coordinate[1], lng: coordinate[0]}
-                })
-                searchCoordinates = searchCoordinates.filter((coordinate, index) => index % 100 === 0)
-                getPOIs(searchCoordinates, POIType).then(res => {
-                    console.log("POI: ", res.data);
-                    setRecPOI(res.data);
-                }).catch(err => {
-                    console.log(err);
-                })
-            } else {
-                setRecPOI([]);
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-
-    }
-
     if (mode === 'preset'){
         return (
 
@@ -125,10 +57,7 @@ export default function SidePanel(props) {
                         {/* Routes */}
                     </div>
                     <Button id="preset-route-transition-button" onClick={handleChangeButton} variant="contained" color="primary"><ArrowBack/>Back</Button>
-                    <CustomRouteMaker POIType={[POIType, setPOIType]} chosenPOI={[chosenPOI, setChosenPOI]} handlers={[setStartLocation, setEndLocation,setRecPOI]}/>
-                    <Button id="search-route-button" onClick={handleSearchCustomRoute} color="primary" variant="contained" component="span">
-                        <Search />
-                    </Button>
+                    <CustomRouteMaker recPOI={[recPOI, setRecPOI]} chosenRoute={[chosenRoute,setChosenRoute]} POIType={[POIType, setPOIType]} chosenPOI={[chosenPOI, setChosenPOI]} startLocation={[startLocation, setStartLocation]} endLocation={[endLocation,setEndLocation]} recPOI={[recPOI, setRecPOI]}/>
                     {/* <PresetList presetList={presetList}/> */}
                 </div>
             </div>
