47eac2b873efa47d48c0b43a9252cfabfdd44c95
Remakelo <tio-own96@hotmail.com>
2020-10-28 16:34:57 +0800

    added geolocation

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { GoogleMap, LoadScript, Data, Marker } from '@react-google-maps/api';
+import { LocalDining, FilterHdr, Store } from '@material-ui/icons'
+
 const containerStyle = {
   width: "100%",
   height: "100vh"
 };
+
+const mapStyle = require('assets/style.json');
 
 const center = {
   lat: 1.3878,
@@ -24,56 +28,29 @@ function processPoints(geometry, callback, thisArg) {
   }
 }
 
-
-
 export default function Maps(props) {
   const [route, setRoute] = props.route
   const [startLocation, setStartLocation] = props.startLocation
   const [endLocation, setEndLocation] = props.endLocation
+  const [recPOI, setRecPOI] = props.recPOI
+  const [POIType, setPOIType] = props.POIType
+  const [chosenPOI, setChosenPOI] = props.chosenPOI
+
+  
   const [map, setMap] = React.useState(null)
   const [data, setData] = React.useState(null)
   const [features, setFeatures] = React.useState(null)
-  const[usergps, setusergps] = useState(null)
-
-  navigator.geolocation.getCurrentPosition(
-      function(position) {
-        console.log(position);
-        setusergps( {lon : position.coords.longtitude, lat : position.coords.latitude})
-        console.log("CANCERISNEAR", usergps.lon)
-      },
-      function(error) {
-        console.error("Error Code = " + error.code + " - " + error.message);
-      }
-    );
 
   const onLoad = React.useCallback(function callback(map) {
     setMap(map)
     console.log("Maps:", map)
     const bounds = new window.google.maps.LatLngBounds({ lat: 1.17, lng: 103.6 }, { lat: 1.46, lng: 104.1 });
     map.fitBounds(bounds);
+    map.setOptions({styles: mapStyle})
   }, [])
-
-<<<<<<< Updated upstream
-=======
-
- //function to get user current location
-const [userloc, setloc] = React.useState('lat')
-navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position);
-      setloc({ 
-        lon : position.coords.longitude, 
-        lat : position.coords.latitude
-      });
-     console.log("this is dasdebug: Lat is " + userloc.lat +  " user long is " + userloc.lon);
-    },
-    function(error) {
-      console.error("Error Code = " + error.code + " - " + error.message);
-   }
-  );
 
 
   // runs this function if route changes
->>>>>>> Stashed changes
   useEffect(() => {
     if (features) {
       for (var i = 0; i < features.length; i++)
@@ -82,6 +59,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
     if (data){
       const routeFeature = data.addGeoJson(route)
       setFeatures(routeFeature)
+      data.setStyle({strokeColor: 'red'})
     }
   }, [route])
 
@@ -127,6 +105,20 @@ navigator.geolocation.getCurrentPosition(function(position) {
     setMap(null)
   }, [])
 
+  function handleClickPOI(place) {
+    let index = chosenPOI.findIndex(chosenPlace => chosenPlace === place)
+    if (index === -1) {
+      // the POI has not been chosen
+      setChosenPOI([...chosenPOI, place])
+    } else {
+      // the POI was chosen previously, remove instead
+      let new_chosenPOI = [...chosenPOI]
+      new_chosenPOI.splice(index, 1)
+      setChosenPOI(new_chosenPOI)
+    }
+    setPOIType(null);
+  }
+
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyDXyYgpyHPB77RyblUo6jF7WDMLfH0VeS0"
@@ -144,8 +136,14 @@ navigator.geolocation.getCurrentPosition(function(position) {
           <Data onLoad={onLoadData} onAddFeature={onAddFeatureData}/>
           {/* <Marker position={startLocation} draggable/> */}
           {/* <Marker position={endLocation} draggable/> */}
-          <Marker position={startLocation} draggable onDragEnd={handleDragStartLocation}/>
-          <Marker position={endLocation} draggable onDragEnd={handleDragEndLocation}/>
+          <Marker position={startLocation} draggable onDragEnd={handleDragStartLocation} icon={{ url: require('assets/start.png'), }}/>
+          <Marker position={endLocation} draggable onDragEnd={handleDragEndLocation} icon={{ url: require('assets/end.png'), }}/>
+          {recPOI.map(place => {
+                return <Marker key={place.place_id} position={place.geometry.location} icon={"rec_POI.svg"} title={place.name} onClick={() => handleClickPOI(place)}/>
+            })}
+          {chosenPOI.map(place => {
+                return <Marker key={place.place_id} position={place.geometry.location} title={place.name} onClick={() => handleClickPOI(place)}/>
+            })}
         </>
       </GoogleMap>
     </LoadScript>
