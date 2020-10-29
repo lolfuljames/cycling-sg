ccbd4919f6b601a8ec10c6457e7a905684b85b65
James Tan <jamestancheemin98@gmail.com>
2020-10-29 14:14:17 +0800

    Add Srollbar for Side Panel

--- client/src/components/PresetRouteManager.js
+++ client/src/components/PresetRouteManager.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Paper } from '@material-ui/core';
 import PresetRoute from './PresetRoute'
 import './PresetRouteManager.css'
 
 export default function PresetRouteManager( {presetList, handlers}) {
     return (
         <div className='presetList'>
-            {presetList.map(presetRoute => {
-                return <PresetRoute key={presetRoute.id} route={presetRoute} handlers={handlers}/>
-            })}
+            <Paper style={{maxHeight: "70vh", overflow: 'auto'}}>
+                {presetList.map(presetRoute => {
+                    return <PresetRoute key={presetRoute.id} route={presetRoute} handlers={handlers}/>
+                })}
+            </Paper>
         </div>
     )
 }
